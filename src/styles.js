93659d50dfe742a9a7ace6f068266103a796c694
Nicklas <nicklas_tp@hotmail.com>
2022-05-16 00:16:34 +0200

    commit

--- src/styles.js
+++ src/styles.js
@@ -18,12 +18,16 @@ export default makeStyles((theme) => ({
   last: {
     [theme.breakpoints.down('xs')]: {
       marginBottom: theme.spacing(3),
-      paddingBottom: '4.5%',
+      paddingBottom: '2.5%',
     },
   },
   grid: {
     '& > *': {
+      [theme.breakpoints.down('xs')]: {
+        margin: theme.spacing(1)
+    },
       margin: theme.spacing(2),
     },
+    
   },
 }))
