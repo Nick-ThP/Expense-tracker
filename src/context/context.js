a8f5f7da16316c85e4ae085693fa7572ac361948
Nicklas <nicklas_tp@hotmail.com>
2022-05-16 00:31:58 +0200

    commit

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,7 @@
 import React, { useReducer, createContext } from 'react'
 import contextReducer from './contextReducer'
 
-const initialState = JSON.parse(localStorage.getItem('transactions')) || [{"amount":200,"category":"Lottery","type":"Income","date":"2022-04-10","id":"fb571f4f-aad4-4c10-abde-65d7396f7c90"},{"amount":18000,"category":"Salary","type":"Income","date":"2022-04-10","id":"456cc78a-64bf-4d2d-bf15-8023518f4514"},{"amount":400,"category":"Entertainment","type":"Expense","date":"2022-04-10","id":"2a07ee91-efd3-48d4-862f-955f4eeec171"},{"amount":5600,"category":"Travel","type":"Expense","date":"2022-04-10","id":"1dd86b9a-2d19-4f56-816a-03c8e2e21286"}]
+const initialState = JSON.parse(localStorage.getItem('transactions')) || [{"amount":1400,"category":"Lottery","type":"Income","date":"2022-05-15","id":"c0999dd7-682b-4d15-8b8d-8be430608d6d"},{"amount":7000,"category":"Car","type":"Expense","date":"2022-05-15","id":"05e81e87-c90b-48c2-80f4-fd8228500e51"},{"amount":18000,"category":"Salary","type":"Income","date":"2022-05-10","id":"456cc78a-64bf-4d2d-bf15-8023518f4514"},{"amount":400,"category":"Entertainment","type":"Expense","date":"2022-05-10","id":"2a07ee91-efd3-48d4-862f-955f4eeec171"}]
 
 export const ExpenseTrackerContext = createContext(initialState)
 
